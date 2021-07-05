c44876d17f21884625d2a7b914aaddda76ff15fa
Kurtdesou <Kurtdesou@users.noreply.github.com>
2021-07-05 18:52:58 -0400

    finished animation

--- Bouncing Balls/main.js
+++ Bouncing Balls/main.js
@@ -1,50 +1,116 @@
-let canvas = document.createElement("CANVAS");
-let ctx = canvas.getContext("2d");
-let width = (canvas.width = window.innerWidth);
-let height = (canvas.height = window.innerHeight);
+// setup canvas
 
-function RandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; // max & min both included
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
+
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
 }
 
-function RandomColor(min, max) {
-  let randomColor =
-    "rgb(" +
-    RandomInt(min, max) +
-    "," +
-    RandomInt(min, max) +
-    "," +
-    RandomInt(min, max) +
-    ")";
-  return randomColor;
-}
-function RandomRadius(min, max) {
-  let randomRadius = RandomInt(min, max);
-  return randomRadius;
+// define Ball constructor
+
+function Ball(x, y, velX, velY, color, size) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.color = color;
+  this.size = size;
 }
 
-function RandomLocation(min, max) {
-  let randomLocation = RandomInt(min, max);
-  return randomLocation;
-}
-for (let i = 0; i < 10; i++) {
-  // let canvas = document.createElement("CANVAS");
-  // let ctx = canvas.getContext("2d");
-  // let width = (canvas.width = window.innerWidth);
-  // let height = (canvas.height = window.innerHeight);
+// define ball draw method
 
-  let randomColor = RandomColor(0, 255);
-  let randomRadius = RandomRadius(10, 20);
-  let randomX = RandomLocation(0 + randomRadius, width - randomRadius);
-  let randomY = RandomLocation(0 + randomRadius, height - randomRadius);
-
+Ball.prototype.draw = function () {
   ctx.beginPath();
-  ctx.arc(randomX, randomY, randomRadius, 0, 2 * Math.PI);
-  ctx.stroke();
-  ctx.fillStyle = randomColor;
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
   ctx.fill();
-  document.body.appendChild(canvas);
-  console.log(canvas.width);
+};
+
+// define ball update method
+
+Ball.prototype.update = function () {
+  if (this.x + this.size >= width) {
+    this.velX = -this.velX;
+  }
+
+  if (this.x - this.size <= 0) {
+    this.velX = -this.velX;
+  }
+
+  if (this.y + this.size >= height) {
+    this.velY = -this.velY;
+  }
+
+  if (this.y - this.size <= 0) {
+    this.velY = -this.velY;
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+};
+
+// define ball collision detection
+
+Ball.prototype.collisionDetect = function () {
+  for (let j = 0; j < balls.length; j++) {
+    if (!(this === balls[j])) {
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color =
+          "rgb(" +
+          random(0, 255) +
+          "," +
+          random(0, 255) +
+          "," +
+          random(0, 255) +
+          ")";
+      }
+    }
+  }
+};
+
+// define array to store balls and populate it
+
+let balls = [];
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  let ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the adge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    "rgb(" + random(0, 255) + "," + random(0, 255) + "," + random(0, 255) + ")",
+    size
+  );
+  balls.push(ball);
 }
+
+// define loop that keeps drawing the scene constantly
+
+function loop() {
+  ctx.fillStyle = "rgba(0,0,0,0.25)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].draw();
+    balls[i].update();
+    balls[i].collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
